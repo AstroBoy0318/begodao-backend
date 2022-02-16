bd315e0bc64118cf9362121c10b336818c4ad980
toby0318 <78232000+toby0318@users.noreply.github.com>
2022-02-16 19:35:21 +0900

    updated with new addresses on mainnet

--- app/config/web3.config.js
+++ app/config/web3.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    chainId: 80001,
-    rpcUrl: "https://speedy-nodes-nyc.moralis.io/89b4f5c6d2fc13792dcaf416/polygon/mumbai",
-    wftmAddress: "0x9c3c9283d3e44854697cd22d3faa240cfb032889",
-    daiAddress: "0xef45e6E3159e9F302D2B85f6E777791d7B7e98d8",
-    begoAddress: "0x6fBEe246b7348F02A04348f160583d3799525001",
-    factoryAddress: "0x58767006e1586e1682aeC6E2078B1b3F15Bb836A",
-    stakingAddress: "0x08B03ac83F2da266594AcC6d02D744fBC92351FB",
-    treasuryAddress: "0xc8268553695245aB61d2D6AF31c6FEd13fe1D965",
+    chainId: 250,
+    rpcUrl: "https://rpc.ftm.tools/",
+    wftmAddress: "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83",
+    daiAddress: "0x8d11ec38a3eb5e956b052f67da8bdc9bef8abf3e",
+    begoAddress: "0xe961c722515657C375Fb0D504953041876C34e94",
+    factoryAddress: "0x152ee697f2e276fa89e96742e9bb9ab1f2e61be3",
+    stakingAddress: "0xBD640dF25B53Cd0D5e2958d7ecF0aa608C3B3009",
+    treasuryAddress: "0xB5c72472bef025Cb286388BAA9C35D81880635ee",
     zeroAddress: "0x0000000000000000000000000000000000000000"
 }
